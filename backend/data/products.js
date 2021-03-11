1be51dcd733f0b298f2cd93f3e0c1b55359b95fb
Maciej Kubinski <kubinskimaciej@gmail.com>
2021-03-11 22:59:21 +0100

    implementing backend1

---
+++ backend/data/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: "1",
+    name: "aaaaaaa",
+    brand: "aaaa",
+    price: "999",
+  },
+  {
+    id: "2",
+    name: "bbbbbbb",
+    brand: "bbbb",
+    price: "888",
+  },
+  {
+    id: "3",
+    name: "cccccc",
+    brand: "cccc",
+    price: "777",
+  },
+  {
+    id: "4",
+    name: "ddddddd",
+    brand: "dddd",
+    price: "666",
+  },
+];
+
+export default products;
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p.id === req.params.id);
+  res.json(product);
+});
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+const PORT = process.envPORT || 5000;
+
+app.listen(PORT, console.log(`Server running on ${PORT}`));
